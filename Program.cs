9c3f2b9007aabcbfcff6b77ae703bef185952578
Meggiegem <Meggiegem@users.noreply.github.com>
2016-07-12 13:08:45 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{//Homework Assignment #3
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                TimeSpan t = TimeSpan.FromSeconds(259200);
+                Console.WriteLine(t.Days);
+                Console.WriteLine(t.Hours);
+                Console.WriteLine(t.Minutes);
+                Console.WriteLine(t.Seconds);
+                Console.WriteLine(t.ToString());
+            }
+        }
+    }
+  
